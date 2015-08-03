38fc80106779aafcd745807874ccf1dbcd8266d9
Roopak Burman <roopak.burman@gmail.com>
2015-08-02 23:39:41 -0400

    new view

--- www/js/app.js
+++ www/js/app.js
@@ -565,7 +565,7 @@ function PeopleCtrl($scope, $http) {
   ];
 
 })
-.controller('ButtonsTabCtrl', function ($scope, $ionicModal) {
+.controller('ButtonsTabCtrl', function ($scope, $ionicModal, $ionicActionSheet) {
     $scope.showPopup = function () {
      $ionicPopup.alert({
        title: 'Calling your Account Manager!',
@@ -597,16 +597,16 @@ function PeopleCtrl($scope, $http) {
 
     $scope.showActionsheet = function () {
         $ionicActionSheet.show({
-          titleText: 'Ionic ActionSheet',
+          titleText: 'Select an option',
           buttons: [
             {
-              text: 'Facebook'
+              text: 'Sort by Category'
             },
             {
-              text: 'Twitter'
+              text: 'Sort by Program'
             },
           ],
-          destructiveText: 'Delete',
+          // destructiveText: 'Delete',
           cancelText: 'Cancel',
           cancel: function () {
             console.log('CANCELLED');
@@ -618,9 +618,35 @@ function PeopleCtrl($scope, $http) {
           destructiveButtonClicked: function () {
             console.log('DESTRUCT');
             return true;
-          }
+          },
+		  buttonClicked: function(index) {
+       if(index === 0){ // Manual Button
+         alert('Manual button clicked...');
+       }
+       else if(index === 1){
+        alert('See on Site button clicked...');
+       }
+
+       return true;
+  }
         });
+		
     };
+	
+$(function() {
+	var openBtn = $('#searchBtn'),
+		slideMenu = $('#searchMenu'), 
+		layer = $('<div />').addClass('layer');
+	openBtn.on("click", function() {
+		if (slideMenu.is(':hidden')) {
+			layer.appendTo('body');
+			slideMenu.slideDown(300); 
+		} else {
+			slideMenu.slideUp(300); 
+			layer.remove();
+		}
+	});
+});	
 })
 
 .controller('SlideboxCtrl', function($scope, $ionicSlideBoxDelegate) {
@@ -640,6 +666,41 @@ function PeopleCtrl($scope, $http) {
   });
  })
 
+ /* .controller('MyController', function($scope, $ionicPopover) {
+
+  // .fromTemplate() method
+  var template = '<ion-popover-view><ion-header-bar> <h1 class="title">My Popover Title</h1> </ion-header-bar> <ion-content> Hello! </ion-content></ion-popover-view>';
+
+  $scope.popover = $ionicPopover.fromTemplate(template, {
+    scope: $scope
+  });
+  // .fromTemplateUrl() method
+  $ionicPopover.fromTemplateUrl('my-popover.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hide popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });
+}) */
+ 
+ 
  .controller('AppCtrl', function() {
 
   ionic.Platform.ready(function() {
