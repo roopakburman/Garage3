bbd8d974b4717eb5b55921593411ffef564d18ca
roopakburman <roopak.burman@gmail.com>
2015-08-17 14:54:46 -0400

    updated

--- www/js/app.js
+++ www/js/app.js
@@ -713,8 +713,8 @@ $scope.login = function(email, password){
 			alert('Welcome Julie!');
 		}else if(authData.uid == 'simplelogin:27'){
 			alert('Welcome Matthew King!');
-		}else if(authData.uid == 'simplelogin:28'){
-			alert('Welcome Ponna!');
+		// }else if(authData.uid == 'simplelogin:28'){
+			// alert('Welcome Ponna!');
 		}else if(authData.uid == 'simplelogin:29'){
 			alert('Welcome Fletcher!');
 		}
@@ -753,15 +753,15 @@ $scope.login = function(email, password){
 		"userPhone" : "571-303-5510",
 		"userOffice" : "Washington DC",
 		"uPic" : "img/fletcher.jpg"
-	},{
-		"userID" : "simplelogin:28",
-		"userName" : "Ponna Arumugam",
-		"userEmail" : "PArumugam@cebglobal.com",
-		"userTitle" : "Chief Technology Officer",
-		"userDept" : "IT",
-		"userPhone" : "571-303-5510",
-		"userOffice" : "Washington DC",
-		"uPic" : "img/ponna.jpg"
+	// },{
+		// "userID" : "simplelogin:28",
+		// "userName" : "Ponna Arumugam",
+		// "userEmail" : "PArumugam@cebglobal.com",
+		// "userTitle" : "Chief Technology Officer",
+		// "userDept" : "IT",
+		// "userPhone" : "571-303-5510",
+		// "userOffice" : "Washington DC",
+		// "uPic" : "img/ponna.jpg"
 	},
 	{
 		"userID" : "simplelogin:27",
