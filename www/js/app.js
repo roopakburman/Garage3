7fe3bf54eb6e2ead273acbbefad2c9aaf0fa191e
roopakburman <roopak.burman@gmail.com>
2015-09-14 12:00:18 -0400

    android tabs bottom

--- www/js/app.js
+++ www/js/app.js
@@ -16,7 +16,6 @@ ceb.run(function($ionicPlatform, $rootScope) {
 })
 .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 	$ionicConfigProvider.tabs.position('bottom');
-	
   $stateProvider
     .state('menu', {
       url: "/menu",
